dfc852618acfd8e1a3640e7901fbfae63202328c
risqid <risqidharmawan@gmail.com>
2025-06-05 11:09:38 +0700

    Install package Laravel Activity Log dari SPATIE dan implementasi pada model Persyaratan

--- app/Models/Persyaratan.php
+++ app/Models/Persyaratan.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Persyaratan extends Model
 {
-    use UUID;
+    use UUID, LogsActivity;
 
     protected $fillable = ['nama_persyaratan'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->setDescriptionForEvent(fn(string $eventName) => "Persyaratan has been {$eventName}")
+            ->useLogName('Persyaratan') // Set the log name for the activity
+            ->logOnly(['nama_persyaratan']);
+    }
 }
